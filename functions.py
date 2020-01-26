ec7e314d6b58ed2d48367d8ae116e2c25b3c40ac
samia7 <samia.siddique07@gmail.com>
2020-01-25 23:07:09 -0800

    Create initial skeletal code for graph viewer script
    Script contains 2 modules, 1 for the functions, 1 for the GUI
    Function module contains an abstract Function class with
    run_function and invalid_case methods (subject to modification)
    and skeletal SineWave class (functinality not implemented)

---
+++ functions.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+Script that contains all the functions that can be viewed using the graph viewer UI. 
+The script contains an abstract class Function that is inherited by all the 
+subclasses that have the description for specific functions.
+In order to add new functions to be displayed by the UI, simply create a new class
+describing that function, and extend Functions class.
+For each class inherting Function:
+Name: Defines the name identifier used in the GUI
+A, B: The changable parameters defining the function
+""" 
+
+from abc import ABC, abstractmethod
+
+class Function(ABC):
+    """
+    This is the main abstract class that is inherited by all the function 
+    subclasses.
+    """
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def run_function(self, A, B):
+        """
+        This method defines the operation that is performed by a function.
+        """
+        pass
+    
+    @abstractmethod
+    def invalid_case(self):
+        """
+        This method handles all invalid parameters for A and B
+        """
+        pass
+
+class SineWave(Function):
+    """
+    Name: Sine Wave
+    A: The amplitude of the wave (default amplitude = 1)
+    B: The time period of the wave (default period = 1)
+    """
+    def __init__(self, A=1, B=1):
+        self.A = A
+        self.B = B
+
+    def run_function(self):
+        return (self.A + self.B)
+    
+    def invalid_case(self):
+        return super().invalid_case()
+
---
+++ graphviewer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Script that runs the graph viewer interface and contains the 
+functionality for the the GUI
+""" 
+from functions import *
+
+class GraphViewer:
+    """
+    This class contains the description of the GUI
+    """
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    """
+    Run the graph viewer
+    """
